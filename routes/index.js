92f7a1fa0170abac871fd86013f1a45ec03f251e
Saribeg <saribeg@gmail.com>
2018-12-18 14:10:31 +0200

    memo delete and memo color are ready with ajax

--- routes/index.js
+++ routes/index.js
@@ -9,42 +9,16 @@ router.get('/', ensureAuthenticated, (req, res) => {
 });
 
 router.get('/:user', ensureAuthenticated, (req, res) => {
-	// console.log(req.user);
 
 	Memo.find({username: req.user.username}, (err, docs) => {
 		if(err) return console.log(err);
 
-		/*let testObj = {
-			title: docs[0].memoTitle,
-			links: docs[0].memoLinks,
-			text: docs[0].memoDescription
-		}*/
-
-		/*let arrtest = [];
-
-		docs.forEach(function(item, i, arr){
-			arrtest.push(item['username'] + " : " + item['memoTitle']);
-		});
-
-		res.send(arrtest);*/
-
-		//res.send(docs);
-
-		//let userMemos = docs;
-		//res.send(userMemos);
-
-		// docs.forEach(function(item, i, arr){
-		// 	console.log(item['memoLinks'].length)
-		// });
-		
-
 		res.render('index', {
-			userMemos: docs
+			userMemos: docs.reverse()
 		});
 
 	});
 	
-	//res.render('index');
 });
 
 function ensureAuthenticated(req, res, next){
